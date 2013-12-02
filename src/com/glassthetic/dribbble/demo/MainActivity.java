ea627cec4bb4d8bc0cc0912b1c98d50659d734ea
Paul Young <paulyoungonline@gmail.com>
2013-12-02 00:23:05 -0500

    WIP: Refactoring to make activities more reusable.

--- src/com/glassthetic/dribbble/demo/MainActivity.java
+++ src/com/glassthetic/dribbble/demo/MainActivity.java
@@ -1,37 +1,33 @@
 package com.glassthetic.dribbble.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.os.Parcelable;
 
 import com.glassthetic.dribbble.api.DribbbleAPI;
 import com.glassthetic.dribbble.api.ErrorListener;
 import com.glassthetic.dribbble.api.Listener;
 import com.glassthetic.dribbble.api.Shot;
 
-
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		final DribbbleAPI api = new DribbbleAPI(this.getApplicationContext());
 		
 		api.getPopularShots(new Listener<List<Shot>>() {
 
 			@Override
-			public void onResponse(final List<Shot> shots) {			
-				final ListView listView = (ListView) findViewById(R.id.list);
-				final ShotListArrayAdapter adapter = new ShotListArrayAdapter(MainActivity.this, R.layout.list_item, shots);
-				listView.setAdapter(adapter);
+			public void onResponse(final List<Shot> shots) {
+				ArrayList<? extends Parcelable> arrayListOfShots = (ArrayList<? extends Parcelable>) new ArrayList<Shot>(shots);
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, ShotsActivity.class);
+				intent.putParcelableArrayListExtra("shots", arrayListOfShots);
+				startActivity(intent);
 			}
 		}, new ErrorListener() {
 
@@ -41,32 +37,5 @@ public class MainActivity extends Activity {
 				error.printStackTrace();
 			}
 		});
-	}
-	
-	private class ShotListArrayAdapter extends ArrayAdapter<Shot> {
-		private final Context context;
-		private List<Shot> objects;
-		
-		public ShotListArrayAdapter(Context context, int textViewResourceId, List<Shot> objects) {
-			super(context, textViewResourceId, objects);
-			this.context = context;
-			this.objects = objects;
-		}
-		
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			View rowView = inflater.inflate(R.layout.list_item, parent, false);
-			
-//			ImageView imageView = (ImageView) rowView.findViewById(R.id.image);
-			TextView titleTextView = (TextView) rowView.findViewById(R.id.title);
-			TextView playerNameTextView = (TextView) rowView.findViewById(R.id.player_name);
-			
-			Shot shot = objects.get(position);
-			titleTextView.setText(shot.title);
-			playerNameTextView.setText(shot.player.name);
-			
-			return rowView;
-		}
 	}
 }
---
+++ src/com/glassthetic/dribbble/demo/ShotActivity.java
@@ -0,0 +1,17 @@
+package com.glassthetic.dribbble.demo;
+
+import android.app.Activity;
+//import android.content.Intent;
+import android.os.Bundle;
+
+public class ShotActivity extends Activity {
+
+//	private String title;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		Intent intent = getIntent();
+//		this.title = intent.getStringExtra("title");
+	}
+}
