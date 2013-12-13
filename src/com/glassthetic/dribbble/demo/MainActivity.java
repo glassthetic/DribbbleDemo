a06ab3b152f6de86ac820baf506cd0316c50d096
Paul Young <paulyoungonline@gmail.com>
2013-12-12 22:04:59 -0500

    Updated to use new API init method.

--- src/com/glassthetic/dribbble/demo/MainActivity.java
+++ src/com/glassthetic/dribbble/demo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		DribbbleAPI.setContext(this.getApplicationContext());
+		DribbbleAPI.init(this.getApplicationContext());
 		
 		Shot.getPopular(new Listener<List<Shot>>() {
 
